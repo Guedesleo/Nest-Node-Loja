43ddd15e217415dc5a855489f2d8f331ab25654a
Guedesleo1 <leonardoguedesrocha95@gmail.com>
2024-03-05 19:03:51 -0300

    Persistencia de Banco de dados Typeorm: 01-Config Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './Users/users.module';
 import { ProductsModule } from './Products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [
+    UsersModule, 
+    ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+    useClass: PostgresConfigService,
+    inject: [PostgresConfigService],
+  }),
+],
   controllers: [],
   providers: [],
 })
